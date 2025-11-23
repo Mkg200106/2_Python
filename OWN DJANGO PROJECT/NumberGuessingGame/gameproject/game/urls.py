469a51f9810da623aa0e360fb942c088868d632e
Mkg200106 <martharamphele01@gmail.com>
2025-11-23 09:37:53 -0800

    Delete OWN DJANGO PROJECT/NumberGuessingGame/gameproject directory

--- OWN DJANGO PROJECT/NumberGuessingGame/gameproject/game/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.guess_number, name='guess_number'),
-    path('reset/', views.reset_game, name='reset_game'),
-]
